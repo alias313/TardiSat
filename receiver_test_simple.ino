778daaa626e234c4fa021f884069ff2f57964c8a
Elies Garcia Alvira <68759092+alias313@users.noreply.github.com>
2023-04-18 19:55:07 +0200

    Update receiver_test_simple.ino

--- receiver_test_simple.ino
+++ receiver_test_simple.ino
@@ -16,7 +16,6 @@
 
 #define OLED_SDA 4
 #define OLED_SCL 15 
-#define OLED_RST 16
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
